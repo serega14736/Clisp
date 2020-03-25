03d8844f11e42cee87e559c414ea84def79c05c8
prokofev serega <40569614+serega14736@users.noreply.github.com>
2020-03-25 12:06:45 +0300

    Update clisp.lisp

--- clisp.lisp
+++ clisp.lisp
@@ -13,6 +13,7 @@
 (princ " >> '(4 2 3) '(7 3 7)")
 (print(scalar '(4 2 3) '(7 3 7)))
 (write-line "")
+(write-line "")
 
 ;;; Test 2
 
@@ -20,6 +21,9 @@
 (princ " >> '(0 1 2) '(1 1 1)")
 (print(scalar '(0 1 2) '(1 1 1)))
 (write-line "")
+(write-line "")
+
+
 
 ; 19
 ; Определите функцию (ЛУКОВИЦА n), строящую N-уровневый вложенный список, элементом которого на самом глубоком уровне является N.
@@ -38,12 +42,16 @@
 (princ " >> 5 ")
 (print (lis '5))
 (write-line "")
+(write-line "")
+
 
 ;;; Test 2
 (write-line "Задача 19 Test 2")
 (princ " >> 0")
 (print (lis '0))
 (write-line "")
+(write-line "")
+
 
 ;18 
 ;Определите предикат, проверяющий, является ли аргумент одноуровневым списком.
@@ -62,11 +70,15 @@
 (princ " >> ((3)) ")
 (print(spisok '((3))))
 (write-line "")
+(write-line "")
+
 
 (write-line "Задача 18 Test 2")
 (princ " >> '(1 2 3) ")
 (print(spisok '(1 2 3)))
 (write-line "")
+(write-line "")
+
 
 ;22
 ;Определите функцию, которая обращает список (а b с) и разбивает его на уровни (((с) b) а).
@@ -82,4 +94,39 @@
 (write-line "Задача 22 Test 1")
 (princ " >> '( 1 2  3 4 5 6  7 8  9 ) ")
 (print(func-level '( 1 2  3 4 5 6  7 8  9 )))
+(write-line "")
+(write-line "")
+
+
+;9
+;Определите функцию, разделяющую исходный список на два подсписка. 
+;В первый из них должны попасть элементы с нечетными номерами, 
+;во второй — элементы с четными номерами.
+;(1 2 3) -> ((1 3) (2))
+(defun list-sublist(lst)
+(cond
+	((null (cdr lst)) (list lst))
+	(t 
+		((lambda (lst1)
+		(list
+			(cons (car lst) (car lst1))
+			(cons (cadr lst) (cadr lst1))))
+		(list-sublist (cddr lst)))
+	)
+)
+)
+
+;;; Test 1
+(write-line "Задача 9 Test 1")
+(princ " >> (1 2 3) ")
+(print(list-sublist`(1 2 3)))
+(write-line "")
+(write-line "")
+
+
+;;; Test 1
+(write-line "Задача 9 Test 1")
+(princ " >> (1 2 3 4 5 6 7 8) ")
+(print(list-sublist`(1 2 3 4 5 6 7 8)))
+(write-line "")
 (write-line "")
