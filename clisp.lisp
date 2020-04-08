99362bb29befe1aa489a04e6575510ccdc75c721
prokofev serega <40569614+serega14736@users.noreply.github.com>
2020-04-08 11:56:57 +0300

    Update clisp.lisp

--- clisp.lisp
+++ clisp.lisp
@@ -103,23 +103,17 @@
 ;В первый из них должны попасть элементы с нечетными номерами, 
 ;во второй — элементы с четными номерами.
 ;(1 2 3) -> ((1 3) (2))
-(defun list-sublist(lst)
-(cond
-	((null (cdr lst)) (list lst))
-	(t 
-		((lambda (lst1)
-		(list
-			(cons (car lst) (car lst1))
-			(cons (cadr lst) (cadr lst1))))
-		(list-sublist (cddr lst)))
-	)
+(defun sub-lists(lst)
+      (cond
+         ((null lst) '(() ()))
+         ( (evenp (car lst)) (cons (car (sub-lists(cdr lst))) (cons (cons (car lst)   (cadr (sub-lists(cdr lst))))())))
+         (t(cons (cons (car lst) (car (sub-lists(cdr lst))))   (cdr (sub-lists(cdr lst)))))
+       )
 )
-)
-
 ;;; Test 1
 (write-line "Задача 9 Test 1")
 (princ " >> (1 2 3) ")
-(print(list-sublist`(1 2 3)))
+(print(sub-lists`(1 2 3)))
 (write-line "")
 (write-line "")
 
@@ -127,6 +121,6 @@
 ;;; Test 2
 (write-line "Задача 9 Test 1")
 (princ " >> (1 2 3 4 5 6 7 8) ")
-(print(list-sublist`(1 2 3 4 5 6 7 8)))
+(print(sub-lists`(1 2 3 4 5 6 7 8)))
 (write-line "")
 (write-line "")
