0ffb7b9d150bdf4bffd70b805cc7d6a8d0ead867
prokofev serega <40569614+serega14736@users.noreply.github.com>
2020-05-27 10:10:10 +0300

    Update READY_haskell.hs

--- READY_haskell.hs
+++ READY_haskell.hs
@@ -8,3 +8,11 @@ main = do
 
 print $ a [1,2,3,4,1] 1 3
 -- [3,2,3,4,3]
+
+-- 13. Реализовать на языке Haskell функцию, которая, чередуя элементы списков [a, b, ...] и [1, 2, ...], образует новыи? список [a, 1, b, 2, ...].
+mergelst [] x = if x == [] then x else mergelst x []
+mergelst (x:xs) y = x : mergelst y xs
+
+main = do
+print $ mergelst ["1", "2", "3"] ["a", "b", "c"]
+print $ mergelst ["1", "2", "3"] ["a"]
